07947060adb4f422ecf86eee2b91555ce4e54eaf
guhachandrangshu-pixel <guhachandrangshu@gmail.com>
2026-03-21 11:48:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 function predict() {
   let weather = document.getElementById("weather").value;
+  let train = document.getElementById("trainNumber").value;
+
   let delay = 0;
 
-  if (weather === "rain") delay = 25;
-  else if (weather === "fog") delay = 40;
+  if (weather === "rain") delay += 20;
+  if (weather === "fog") delay += 30;
+
+  // random factor
+  delay += Math.floor(Math.random() * 10);
+
+  let status = delay > 0 ? "Delayed" : "On Time";
 
   document.getElementById("result").innerText =
-    "Expected Delay: " + delay + " minutes";
+    "Train " + train + " is " + status + 
+    ". Expected Delay: " + delay + " minutes";
 }
